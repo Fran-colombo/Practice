1479bf54f9c1965866e9877a225d96fb7fd6bd1b
Francesco_1974 <francescocolombo36@gmail.com>
2025-09-28 23:31:32 -0300

    Mariaan

---
+++ Practice/Controllers/MarianController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarianController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok("Marian, te como todo el culo!!");
+        }
+    }
+}
