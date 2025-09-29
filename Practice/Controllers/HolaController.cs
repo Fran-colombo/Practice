d8d6a38a1372d62d0283b180b5eb6525293e1f6d
Francesco_1974 <francescocolombo36@gmail.com>
2025-09-28 22:53:41 -0300

    hola

---
+++ Practice/Controllers/HolaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HolaController : Controller
+    {
+
+        public IActionResult Get()
+        {
+            return Ok("Hola angi, te amo muchoo!");
+        }
+    }
+}
